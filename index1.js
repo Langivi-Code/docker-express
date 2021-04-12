510b19437bb652fc1cad49e9fc34d020992f2f40
unknown <you@example.com>
2021-04-13 00:41:25 +0300

    lower

--- index1.js
+++ index1.js
@@ -61,7 +61,40 @@ console.log(" console.log(test.bind(obj.prop)())")
 //task5
 console.log( 'sayName.call(dog)');
 //task6
-console.log('getUserData.call(userData),getUserData.apply(userData)')
+console.log('getUserData.call(userData),getUserData.apply(userData)');
+//task 7
+function devide (chisl,znamen) {
+    if (znamen==0) {
+        throw new Error ("Помилка, спроба поділити на 0")
+    }
+    return chisl/znamen
+}
+// task 8
+console.log('мені здається що обидва додатки зроблять очистять робочий простір')
+// task 9
+let arr9=['lion','cat','dog','croco','bear','tiger','snake'];
+
+
+function inIndex (index) {
+    try {
+    if (index>=arr9.length||index<0) {
+        throw new TypeError()
+    }
+   for (let i=0;i<arr9.length;i++) {
+       if (index==i) {
+         console.log(`показую ${arr9[i]}`)
+       }
+
+   }
+
+}
+catch(e) {
+    console.error(`${e} введіть індекс від 0 до ${arr9.length}`)
+}
+}
+
+console.log(inIndex(3));
+console.log(inIndex(9));
 
 
 
